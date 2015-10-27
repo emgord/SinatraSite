33535ee5392a93c6fe76c5000ed876f2ff6329a2
emgord <emgord@gmail.com>
2015-10-27 16:38:11 -0700

    added in pages from website from yesterday

--- my-site.rb
+++ my-site.rb
@@ -5,6 +5,18 @@ class MySite < Sinatra::Base
   get "/" do
     erb :index
   end
+  get "/about.html" do
+    erb :about
+  end
+  get "/blog.html" do
+    erb :blog
+  end
+  get "/projects.html" do
+    erb :projects
+  end
+  get "/spookycastle.html" do
+    erb :spookycastle
+  end
   get "/room1.html" do
     erb :room1
   end
