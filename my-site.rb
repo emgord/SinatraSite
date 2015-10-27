3cf367ecf82972e38e1ca47760540f1077acef31
emgord <emgord@gmail.com>
2015-10-27 14:55:08 -0700

    added spooky castle with 4 room pages

--- my-site.rb
+++ my-site.rb
@@ -5,4 +5,16 @@ class MySite < Sinatra::Base
   get "/" do
     erb :index
   end
+  get "/room1.html" do
+    erb :room1
+  end
+  get "/room2.html" do
+    erb :room2
+  end
+  get "/room3.html" do
+    erb :room3
+  end
+  get "/room4.html" do
+    erb :room4
+  end
 end
