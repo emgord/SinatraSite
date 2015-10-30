b9681802adc4a1b35a38be7884a09ec11abfa22c
emgord <emgord@gmail.com>
2015-10-30 10:34:26 -0700

    added navigation buttons and copyright

--- my-site.rb
+++ my-site.rb
@@ -3,33 +3,39 @@ require "sinatra"
 class MySite < Sinatra::Base
 
   get "/" do
+    @room = 0
     @header = "Welcome to the Spooky Castle"
     erb :spookycastle
   end
 
   get "/spookycastle.html" do
+    @room = 0
     @header = "Welcome to the Spooky Castle"
     erb :spookycastle
   end
 
   get "/room1.html" do
-    @header = "Cats!"
+    @room = 1
+    @header = "Spooky Cats!"
     erb :room1
   end
 
   get "/room2.html" do
-    @header = "Owls"
+    @room = 2
+    @header = "Spooky Owls!"
     erb :room2
   end
 
   get "/room3.html" do
-    @header = "Monster"
+    @room = 3
+    @header = "Spooky Monster!"
     erb :room3
   end
 
   get "/room4.html" do
+    @room = 4
     @pumpkins = rand(2..10)
-    @header = "#{@pumpkins} Pumpkins"
+    @header = "#{@pumpkins} Spooky Pumpkins!"
     erb :room4
   end
 
