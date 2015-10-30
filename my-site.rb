4a4d2e2ce178820ece6290388542caa99e52082c
emgord <emgord@gmail.com>
2015-10-30 09:28:09 -0700

    archived about me page to focus on the spooky castle

--- my-site.rb
+++ my-site.rb
@@ -3,39 +3,34 @@ require "sinatra"
 class MySite < Sinatra::Base
 
   get "/" do
-    erb :index
-  end
-
-  get "/about.html" do
-    erb :about
-  end
-
-  get "/blog.html" do
-    erb :blog
-  end
-
-  get "/projects.html" do
-    erb :projects
+    @header = "Welcome to the Spooky Castle"
+    erb :spookycastle
   end
 
   get "/spookycastle.html" do
+    @header = "Welcome to the Spooky Castle"
     erb :spookycastle
   end
 
   get "/room1.html" do
+    @header = "Cats!"
     erb :room1
   end
 
   get "/room2.html" do
+    @header = "Owls"
     erb :room2
   end
 
   get "/room3.html" do
+    @header = "Monster"
     erb :room3
   end
 
   get "/room4.html" do
+    @pumpkins = rand(2..10)
+    @header = "#{@pumpkins} Pumpkins"
     erb :room4
   end
-  
+
 end
